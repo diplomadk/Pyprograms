b443add3e1215e7f369f439c651c259553198e5b
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 19:21:33 +0530

    Delete #6write a program to accpet a number from a users and calculate the sum of all the numbers from 1 to a given number ?.py

--- #6write a program to accpet a number from a users and calculate the sum of all the numbers from 1 to a given number ?.py
+++
@@ -1,9 +0,0 @@
-#program to accpet a number from a users and calculate the sum of all the numbers from 1 to a given number ?
-# s : store sum of all numbers
-s = 0
-n = int(input("Enter number: "))
-#run loop n times
-#stop: n+1
-for i in range(1, n+1, 1):
-    #add current number to sum
-    s += i
