f41542c886174cca31c454faf7963005dd8b92d1
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 18:51:52 +0530

    Create #3write a program to check print the calendar of a given month and year ?.py

---
+++ #3write a program to check print the calendar of a given month and year ?.py
@@ -0,0 +1,5 @@
+#program to check print the calendar of a given month and year ?
+import calendar
+y = int(input("Input The Year: "))
+m = int(input("Input The Month: "))
+print(calendar.month(y,m))
