a95682fe1113157ba491d346a5cd6da325b76a92
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 18:54:42 +0530

    Create #4write a python program to check whether a variable is integer or string ?.py

---
+++ #4write a python program to check whether a variable is integer or string ?.py
@@ -0,0 +1,4 @@
+#program to check whether a variable is integer or string ?
+print(isinstance(25, int)) or isinstance(25, str)
+print(isinstance([25], int)) or isinstance([25], str)
+print(isinstance("25", int)) or isinstance("25", str)
