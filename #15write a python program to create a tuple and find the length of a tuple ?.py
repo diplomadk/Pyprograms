be896558d909199531ff1a4af174f593ef110bc0
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 19:54:50 +0530

    Create #15write a python program to create a tuple and find the length of a tuple ?.py

---
+++ #15write a python program to create a tuple and find the length of a tuple ?.py
@@ -0,0 +1,6 @@
+#program to create a tuple and find the length of a tuple ?
+#create a tuple
+x = ("POLYTECHNIC")
+print("original tuple", x)
+#use the len() function to known the length
+print("Length of a tuple =", len(x))
