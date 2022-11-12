d2cbdd77d4578d9213ed100145b73883b5461671
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 16:02:18 +0530

    Update and rename python1.py to vowelOrnonvwl.py

--- python1.py
+++
@@ -1,2 +0,0 @@
-print("oneway")
-hello='hai bro'
---
+++ vowelOrnonvwl.py
@@ -0,0 +1,8 @@
+#program to check character is Vowel or not
+
+ch=input("Enter Any Character")
+vow="aeiouAEIOU"
+if ch in vow:
+    print("Print Character is Vowel")
+else:
+    print("Print Character is not vowel")
