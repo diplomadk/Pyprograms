72d37439f1c2bbae4ae20047de988c39a3f82d79
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 19:47:14 +0530

    Create #11write a program to find maximum and minimum values in a set ?.py

---
+++ #11write a program to find maximum and minimum values in a set ?.py
@@ -0,0 +1,10 @@
+#program to find maximum and minimum values in a set ?
+#Create a set
+setn={5, 10, 3, 15, 2, 20}
+print("Original set elements: ")
+print(setn)
+print(type(setn))
+print("Maximum value of the said set: ")
+print(max(setn))
+print("Minimum value of the said set: ")
+print(min(setn))
