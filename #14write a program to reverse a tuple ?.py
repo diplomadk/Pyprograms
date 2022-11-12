a5d11c29c6a8d6fb4c00ad87d3408a4957f91356
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 19:53:11 +0530

    Create #14write a program to reverse a tuple ?.py

---
+++ #14write a program to reverse a tuple ?.py
@@ -0,0 +1,11 @@
+#program to reverse a tuple ?
+#create a tuple
+x=("POLYTECHNIC")
+#reverse the tuple
+y=reversed(x)
+print(tuple(y))
+#create another tuple
+x=(5, 10, 15, 20)
+#reversed the tuple
+y=reversed(x)
+print(tuple(y))
