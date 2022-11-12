724d94cd916180f4b276de3bc3a2e1de162b2774
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 18:26:02 +0530

    Update and rename vowelOrnonvwl.py to #program-to-check-whether the given values could construct-square-or-rectangle

--- vowelOrnonvwl.py
+++
@@ -1,8 +0,0 @@
-#program to check character is Vowel or not
-
-ch=input("Enter Any Character")
-vow="aeiouAEIOU"
-if ch in vow:
-    print("Print Character is Vowel")
-else:
-    print("Print Character is not vowel")
