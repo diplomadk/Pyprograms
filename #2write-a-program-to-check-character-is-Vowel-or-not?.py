a7c08ecedcffffd503578ba11d2ac9a47c544645
diplomadk <118050687+diplomadk@users.noreply.github.com>
2022-11-12 18:39:58 +0530

    Create #2write-a-program-to-check-character-is-Vowel-or-not?.py

---
+++ #2write-a-program-to-check-character-is-Vowel-or-not?.py
@@ -0,0 +1,14 @@
+#program to check character is Vowel or not?
+ch=input("Enter Any Character")
+vow="aeiouAEIOU"
+if ch in vow:
+    print("Print Character is Vowel")
+else:
+    print("Print Character is not vowel")
+	
+3. write a program to check print the calendar of a given month and year ?
+
+#rogram to check print the calendar of a given month and year ?
+import calendar
+y = int(input("Input The Year: "))
+m = int(input("Input The Month: "))
